6d99e7c039cb2570799e8fca93e4717a14e310e7
Youssef Rekik <rekik.ysf@gmail.com>
2016-06-06 13:35:38 +0200

    Update AgentMedicalBean.java

--- PCD/src/java/model/AgentMedicalBean.java
+++ PCD/src/java/model/AgentMedicalBean.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 import static com.mchange.v2.c3p0.impl.C3P0Defaults.password;
 import com.sun.istack.internal.NotNull;
